e7553665ad63e5b9384be34061c78190e2650288
Patrick Lascombe <patrick@patrick-XPS-13-9370>
2019-01-04 16:50:09 +0100

    First commit

---
+++ srv_test-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp ~/Lin_srv_test/srv_test /usr/local/bin/srv_test
+sudo chmod 755 /usr/local/bin/nm-watcher
+sudo cp ~/Lin_srv_test/srv_test.service /etc/systemd/system/srv_test.service
+sudo systemctl enable srv_test.service
+sudo service srv_test start
+
