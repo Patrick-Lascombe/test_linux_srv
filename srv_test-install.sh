087ab48c6869dfed748fac0e74b6c27e89c59127
Patrick Lascombe <patrick@patrick-XPS-13-9370>
2019-01-07 15:17:40 +0100

    Working in gnome terminal, last problem is firefox is not opening with root session

--- srv_test-install.sh
+++ srv_test-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cp ~/Lin_srv_test/srv_test /usr/local/bin/srv_test
-sudo chmod 755 /usr/local/bin/nm-watcher
+sudo chmod 755 /usr/local/bin/srv_test
 sudo cp ~/Lin_srv_test/srv_test.service /etc/systemd/system/srv_test.service
 sudo systemctl enable srv_test.service
 sudo service srv_test start
